970d4c8de202d137e75b0674cab8ec32d6eade20
Abhish Abraham <abhish002@gmail.com>
2020-03-15 17:46:27 -0400

    Updates

---
+++ precache-manifest.14fd83f968552353479f9c02fc7f9e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3274bdf0274a9ae809f2e5cf91d1c8b",
+    "url": "/bookaholic/index.html"
+  },
+  {
+    "revision": "c76196908847717da9fb",
+    "url": "/bookaholic/static/css/main.ff5c5369.chunk.css"
+  },
+  {
+    "revision": "3767483a67285da71d9c",
+    "url": "/bookaholic/static/js/2.6833143e.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/bookaholic/static/js/2.6833143e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76196908847717da9fb",
+    "url": "/bookaholic/static/js/main.352a36ea.chunk.js"
+  },
+  {
+    "revision": "ed871ff5e50a4f571378",
+    "url": "/bookaholic/static/js/runtime-main.5373bdfb.js"
+  }
+]);
